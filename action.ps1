e76dea0547fa29faa9f424833541bf018f734b7a
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2023-04-11 21:55:55 +0200

    Update action.ps1

--- action.ps1
+++ action.ps1
@@ -76,7 +76,7 @@ function Set-Variables {
                 echo "$variableName=$propValue" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
                 
                 if ($outputs) {
-                    echo "$variableName=$propValue" >> $env:GITHUB_OUTPUT
+                    echo "$variableName=$propValue" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf8 -Append
                 }
             }
         }
