d4a506c2c669ea99dd4548ce6907062ad7d61908
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2023-04-12 17:04:39 +0200

    changed -join to json array

--- action.ps1
+++ action.ps1
@@ -64,7 +64,7 @@ function Set-Variables {
             }
             else {
                 if ($propValue.GetType().FullName -eq "System.Object[]") {
-                    $propValue = $propValue -join $ArraySeparator
+                    $propValue = $propValue | ConvertTo-Json
                 }
 
                 $variableName = $prop.Name
